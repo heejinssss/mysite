e3b5c8713b105aea38d3ab3458a20d9f228e6976
heejinssss <heejin.dev33@gmail.com>
2024-06-04 10:45:16 +0900

    Feat: MySite3-GuestbookRepository

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -15,6 +15,20 @@ import com.poscodx.mysite.vo.GuestbookVo;
 @Repository
 public class GuestbookRepository {
 
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.208:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+
+		return conn;
+	}
+
 	public int deleteByNoAndPassword(Long no, String password) {
 		int result = 0;
 
@@ -82,20 +96,5 @@ public class GuestbookRepository {
 		}
 
 		return result;
-	}
-
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			String url = "jdbc:mariadb://192.168.0.208:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		}
-
-		return conn;
 	}
 }
