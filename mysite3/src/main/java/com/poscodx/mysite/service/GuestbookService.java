fd2ff8007c093757b4e9e7008814504f34336e3c
heejinssss <heejin.dev33@gmail.com>
2024-06-04 10:27:55 +0900

    Feat: MySite3-GuestBook

--- mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
+++ mysite3/src/main/java/com/poscodx/mysite/service/GuestbookService.java
@@ -2,20 +2,27 @@ package com.poscodx.mysite.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.poscodx.mysite.repository.GuestbookRepository;
+import com.poscodx.mysite.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
-	
-//	public List<GuestbookVo> getContentsList() {
-//		return null;
-//	}
-	
-	public void deleteContents(Long no, String password) {
-		
+
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+
+	public List<GuestbookVo> getContentsList() {
+		return guestbookRepository.findAll();
 	}
-	
-//	public void addContents(GuestbookVo vo) {
-//		
-//	}
+
+	public void deleteContents(Long no, String password) {
+		guestbookRepository.deleteByNoAndPassword(no, password);
+	}
+
+	public void addContents(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+	}
 }
